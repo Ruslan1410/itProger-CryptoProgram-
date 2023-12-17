15fa59d35d5f845caaff60b30c4ddade1bcf96df
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-12-17 10:26:58 +0300

    program crypto

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>CRYPTO</h1>
+  <Input />
+</template>
+
+<script>
+import Input from './components/Input.vue'
+
+export default {
+    components: { Input }
+}
+
+
+</script>
+
+
+
+<style scoped>
+
+</style>
---
+++ src/components/Input.vue
@@ -0,0 +1,24 @@
+<template>
+    <input type="number">
+</template>
+    
+<script>
+export default {
+
+}
+</script>
+    
+    
+    
+<style scoped>
+    input {
+        outline: none;
+        text-align: center;
+        border-radius: 3px;
+        border: 0;
+        padding: 10px 15px;
+        background: #fafafa;
+        color: #333;
+    }
+</style>
+    
